60d59c876e4701a47b831f8e8d2415f15494ec99
YarinKalfon45 <yarinkalfon45@icloud.com>
2023-11-20 20:37:40 +0200

    More code added

--- send_mail.py
+++ send_mail.py
@@ -11,6 +11,7 @@ def send_mail(message, reciver='Yarinkalfon45@gmail.com'):
     # password = os.getenv("Password")
     context = ssl.create_default_context()
     with smtp.SMTP_SSL(host, port, context=context) as server:
+        server.ehlo_or_helo_if_needed()
         server.login(uname, password)
         server.sendmail(uname, reciver, message)
 
