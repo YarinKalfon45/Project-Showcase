3c98bffa9ecf8f63076d9cd36c22c9b207f03752
YarinKalfon45 <yarinkalfon45@icloud.com>
2023-11-20 19:44:32 +0200

    More code added

--- pages/contact_us.py
+++ pages/contact_us.py
@@ -2,17 +2,21 @@ import streamlit as st
 import send_mail as sm
 
 st.header("Contact me")
+options = ["Teach me how to live like you", "Tell me more about your program","Other"]
 
 with st.form(key="Email_form"):
-    usr_email = st.text_input("Your Email address:")
-    raw_message = st.text_area("Your message:")
+    usr_email = st.text_input("Your Email address:",key="usr_mail")
+    select = st.selectbox(label="Which topic you would like to discuss?",options=options)
+    raw_message = st.text_area("Your message:",key="raw_message")
     submit_button = st.form_submit_button()
     message = f"""\
 Subject: You have a new message From {usr_email}
 
 From: {usr_email}
+Topic: {select}
 {raw_message}
     """
 if submit_button:
     sm.send_mail(message)
     st.info("Your message has been submitted.I will contact you soon.Thank you!")
+
--- send_mail.py
+++ send_mail.py
@@ -1,11 +1,19 @@
 import smtplib as smtp
 import ssl
-def send_mail(message,reciver='Yarinkalfon45@gmail.com'):
+import os
+
+
+def send_mail(message, reciver='Yarinkalfon45@gmail.com'):
     host = 'smtp.gmail.com'
     port = 465
     uname = 'Yarinkalfon45@gmail.com'
-    password = 'enlmypwchxzcjwrw'
+    password = os.getenv("Password")
     context = ssl.create_default_context()
-    with smtp.SMTP_SSL(host,port,context=context)as server:
-        server.login(uname,password)
-        server.sendmail(uname,reciver,message)
+    with smtp.SMTP_SSL(host, port, context=context) as server:
+        server.login(uname, password)
+        server.sendmail(uname, reciver, message)
+
+
+"""
+enlmypwchxzcjwrw
+"""
