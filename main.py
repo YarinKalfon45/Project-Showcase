21efdf171e46c49c77e6c0b2a5321b7098854e69
YarinKalfon45 <yarinkalfon45@icloud.com>
2023-11-20 17:16:23 +0200

    changing photos

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ col1, col2 = st.columns(2)
 
 with col1:
     st.image("images/IMG_1309.jpeg")
+
+with col2:
+    st.header("Yarin Kalfon")
+    st.info("""Hi, My name is Yarin.\n\n I'm a 3rd year student at Ben-Gurion University.\n\nThis website is created to
+     showcase my programing skills.
+            """)
+
+st.write("If you have any more questions feel free to contact me in the link down below!")
+
