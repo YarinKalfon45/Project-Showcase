82ec099b2c12bb4e78f6c9d7dbca30746c07911e
YarinKalfon45 <yarinkalfon45@icloud.com>
2023-11-21 15:54:45 +0200

    Bug fixes for emailing from streamlit cloud

--- send_mail.py
+++ send_mail.py
@@ -11,10 +11,18 @@ def send_mail(message, reciver='Yarinkalfon45@gmail.com'):
     port_st = 587
     uname = streamlit.secrets("uname")
     password = streamlit.secrets("password")
+
+    server = smtp.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(uname, password)
+    server.sendmail(uname, reciver,message)
+    server.quit()
+
+
     # server = smtp.SMTP(host,port_st)
-    with smtp.SMTP(host,port_st) as server:
-        server.login(uname,password)
-        server.sendmail(uname,reciver,message)
+    # with smtp.SMTP(host,port_st) as server:
+    #     server.login(uname,password)
+    #     server.sendmail(uname,reciver,message)
 
     # context = ssl.create_default_context()
     # with smtp.SMTP_SSL(host, port, context=context) as server:
