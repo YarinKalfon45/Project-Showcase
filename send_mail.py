9c74ae2fb5bf43cc16483b86a99b5c24728c1936
YarinKalfon45 <yarinkalfon45@icloud.com>
2023-11-21 15:40:35 +0200

    More code added

--- send_mail.py
+++ send_mail.py
@@ -2,13 +2,14 @@ import smtplib as smtp
 import ssl
 import os
 
+import streamlit
+
 
 def send_mail(message, reciver='Yarinkalfon45@gmail.com'):
     host = 'smtp.gmail.com'
     port = 465
-    uname = 'contactyarin@gmail.com'
-    password = os.getenv('Password')
-    # password = os.getenv("Password")
+    uname = streamlit.secrets("uname")
+    password = streamlit.secrets("password")
     context = ssl.create_default_context()
     with smtp.SMTP_SSL(host, port, context=context) as server:
         server.ehlo_or_helo_if_needed()
